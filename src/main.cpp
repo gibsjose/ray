c1829ff2ab7fe4fd862a34e4fa269df4100cf398
Joe Gibson <gibsjose@mail.gvsu.edu>
2019-03-03 15:52:17 -0500

    Move methods to utilities source/header

--- src/main.cpp
+++ src/main.cpp
@@ -28,13 +28,11 @@
 #include "ray.h"
 #include "sphere.h"
 #include "hittable_list.h"
+#include "utilities.h"
 
 ///////////////////////////////////////////////////////////////////////////////
 // METHODS
 ///////////////////////////////////////////////////////////////////////////////
-vec3 RandomInUnitSphere();
-vec3 Colour(const Ray & ray, Hittable * world);
-
 int main() {
     const uint32_t width = 1200;            ///< Scene width
     const uint32_t height = 600;            ///< Scene height
@@ -79,33 +77,5 @@ int main() {
 
             std::cout << ir << " " << ig << " " << ib << "\n";
         }
-    }
-}
-
-// Generate a random vector in a unit sphere
-vec3 RandomInUnitSphere() {
-    vec3 p(1, 1, 1);
-
-    do {
-        p = 2.0 * vec3(drand48(), drand48(), drand48()) - vec3(1, 1, 1);
-    } while (p.squared_length() >= 1.0);
-
-    return p;
-}
-
-// Generate a colour given a ray and a list of hittable objects
-vec3 Colour(const Ray & ray, Hittable * world) {
-    HitRecord record;
-
-    // Check for a hit using the input ray
-    // NOTE: t_min is set to 0.001 instead of 0 to avoid the 'shadow acne problem'
-    //       by ignoring hits very near zero
-    if (world->hit(ray, 0.001, MAXFLOAT, record)) {
-        vec3 target = record.p + record.normal + RandomInUnitSphere();
-        return 0.5 * Colour(Ray(record.p, target - record.p), world);
-    } else {
-        vec3 unit_direction = unit_vector(ray.direction());
-        float t = 0.5F * (unit_direction.y() + 1.0);
-        return ((1.0 - t) * vec3(1, 1, 1)) + (t * vec3(0.5, 0.7, 1.0));
     }
 }
---
+++ src/utilities.cpp
@@ -0,0 +1,48 @@
+///////////////////////////////////////////////////////////////////////////////
+// FILE: utilities.cpp
+//
+// AUTHORS:
+// Joseph Gibson / <joseph.gibson@nasa.gov>
+///////////////////////////////////////////////////////////////////////////////
+
+///////////////////////////////////////////////////////////////////////////////
+/// @file
+///
+/// @brief  Utilities
+///////////////////////////////////////////////////////////////////////////////
+
+///////////////////////////////////////////////////////////////////////////////
+// INCLUDES
+///////////////////////////////////////////////////////////////////////////////
+#include "utilities.h"
+
+///////////////////////////////////////////////////////////////////////////////
+// METHODS
+///////////////////////////////////////////////////////////////////////////////
+// Generate a random vector in a unit sphere
+vec3 RandomInUnitSphere() {
+    vec3 p(1, 1, 1);
+
+    do {
+        p = 2.0 * vec3(drand48(), drand48(), drand48()) - vec3(1, 1, 1);
+    } while (p.squared_length() >= 1.0);
+
+    return p;
+}
+
+// Generate a colour given a ray and a list of hittable objects
+vec3 Colour(const Ray & ray, Hittable * world) {
+    HitRecord record;
+
+    // Check for a hit using the input ray
+    // NOTE: t_min is set to 0.001 instead of 0 to avoid the 'shadow acne problem'
+    //       by ignoring hits very near zero
+    if (world->hit(ray, 0.001, MAXFLOAT, record)) {
+        vec3 target = record.p + record.normal + RandomInUnitSphere();
+        return 0.5 * Colour(Ray(record.p, target - record.p), world);
+    } else {
+        vec3 unit_direction = unit_vector(ray.direction());
+        float t = 0.5F * (unit_direction.y() + 1.0);
+        return ((1.0 - t) * vec3(1, 1, 1)) + (t * vec3(0.5, 0.7, 1.0));
+    }
+}

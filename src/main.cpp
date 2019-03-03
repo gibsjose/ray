086f96d1bcf34647a18a09e23266254e9ca727ae
Joe Gibson <gibsjose@mail.gvsu.edu>
2019-03-03 15:39:08 -0500

    Add in diffuse material and gamma correction

--- src/main.cpp
+++ src/main.cpp
@@ -32,12 +32,15 @@
 ///////////////////////////////////////////////////////////////////////////////
 // METHODS
 ///////////////////////////////////////////////////////////////////////////////
+vec3 RandomInUnitSphere();
 vec3 Colour(const Ray & ray, Hittable * world);
 
 int main() {
-    const uint32_t width = 1200;        ///< Scene width
-    const uint32_t height = 600;        ///< Scene height
-    const uint32_t num_samples = 80;    ///< Number of samples over which to average edge colour
+    const uint32_t width = 1200;            ///< Scene width
+    const uint32_t height = 600;            ///< Scene height
+    const uint32_t num_samples = 80;        ///< Number of samples over which to average edge colour
+    const float gamma = 2.0;                ///< Gamma value
+    const float inv_gamma = 1.0 / gamma;    ///< Inverse gamma value
 
     float aspect_ratio = (float)width / height;
 
@@ -68,6 +71,7 @@ int main() {
             }
 
             colour /= float(num_samples);
+            colour = vec3(pow(colour.r(), inv_gamma), pow(colour.g(), inv_gamma), pow(colour.b(), inv_gamma));
 
             int32_t ir = int32_t(255.99 * colour.r());
             int32_t ig = int32_t(255.99 * colour.g());
@@ -78,11 +82,27 @@ int main() {
     }
 }
 
+// Generate a random vector in a unit sphere
+vec3 RandomInUnitSphere() {
+    vec3 p(1, 1, 1);
+
+    do {
+        p = 2.0 * vec3(drand48(), drand48(), drand48()) - vec3(1, 1, 1);
+    } while (p.squared_length() >= 1.0);
+
+    return p;
+}
+
+// Generate a colour given a ray and a list of hittable objects
 vec3 Colour(const Ray & ray, Hittable * world) {
     HitRecord record;
 
-    if (world->hit(ray, 0.0, MAXFLOAT, record)) {
-        return 0.5 * vec3(record.normal.x() + 1.0, record.normal.y() + 1.0, record.normal.z() + 1.0);
+    // Check for a hit using the input ray
+    // NOTE: t_min is set to 0.001 instead of 0 to avoid the 'shadow acne problem'
+    //       by ignoring hits very near zero
+    if (world->hit(ray, 0.001, MAXFLOAT, record)) {
+        vec3 target = record.p + record.normal + RandomInUnitSphere();
+        return 0.5 * Colour(Ray(record.p, target - record.p), world);
     } else {
         vec3 unit_direction = unit_vector(ray.direction());
         float t = 0.5F * (unit_direction.y() + 1.0);

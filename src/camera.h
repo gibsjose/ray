96f5455ffd2899a62d4a04289f87b06e4a239af0
Joe Gibson <gibsjose@mail.gvsu.edu>
2019-03-04 18:47:56 -0500

    Add FOV and aspect ratio to camera class

--- src/camera.h
+++ src/camera.h
@@ -24,11 +24,23 @@
 ///////////////////////////////////////////////////////////////////////////////
 class Camera {
 public:
-    Camera() {
+    ///////////////////////////////////////////////////////////////////////////
+    /// @brief  Camera constructor
+    ///
+    /// @param  vertical_fov - Top-to-bottom field of view in degrees
+    /// @param  aspect_ratio - Ratio of width to height
+    ///////////////////////////////////////////////////////////////////////////
+    Camera(const float vertical_fov, const float aspect_ratio) {
+        // Convert FOV to radians
+        float theta = vertical_fov * M_PI / 180.0;
+
+        float half_height = tan(theta / 2.0);
+        float half_width = aspect_ratio * half_height;
+
         origin = vec3(0, 0, 0);
-        lower_left_corner = vec3(-2, -1, -1);
-        horizontal = vec3(4, 0, 0);
-        vertical = vec3(0, 2, 0);
+        lower_left_corner = vec3(-half_width, -half_height, -1.0);
+        horizontal = vec3(2.0 * half_width, 0, 0);
+        vertical = vec3(0, 2.0 * half_height, 0);
     }
 
     Ray get_ray(const float u, const float v) {

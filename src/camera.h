941a2b9b98c42a8f0234bc4516fd2821a77686d9
Joe Gibson <gibsjose@mail.gvsu.edu>
2019-03-04 19:44:30 -0500

    Add camera aperture/focal distance capabilities

--- src/camera.h
+++ src/camera.h
@@ -18,6 +18,7 @@
 // INCLUDES
 ///////////////////////////////////////////////////////////////////////////////
 #include "ray.h"
+#include "utilities.h"
 
 ///////////////////////////////////////////////////////////////////////////////
 // CLASSES
@@ -27,30 +28,52 @@ public:
     ///////////////////////////////////////////////////////////////////////////
     /// @brief  Camera constructor
     ///
+    /// @param  look_from - The vector from we are looking from (new origin)
+    /// @param  look_at - The vector to where we are looking at
+    /// @param  vup - View up ('vup') vector
     /// @param  vertical_fov - Top-to-bottom field of view in degrees
     /// @param  aspect_ratio - Ratio of width to height
+    /// @param  aperture - The aperture setting
+    /// @param  focal_distance - The focal distance in pixels
     ///////////////////////////////////////////////////////////////////////////
-    Camera(const float vertical_fov, const float aspect_ratio) {
+    Camera(const vec3 & look_from, const vec3 & look_at, const vec3 & vup, const float vertical_fov, const float aspect_ratio, const float aperture, const float focal_distance) {
         // Convert FOV to radians
         float theta = vertical_fov * M_PI / 180.0;
+
+        lens_radius = aperture / 2.0;
+
+        vec3 u;
+        vec3 v;
+        vec3 w;
 
         float half_height = tan(theta / 2.0);
         float half_width = aspect_ratio * half_height;
 
-        origin = vec3(0, 0, 0);
-        lower_left_corner = vec3(-half_width, -half_height, -1.0);
-        horizontal = vec3(2.0 * half_width, 0, 0);
-        vertical = vec3(0, 2.0 * half_height, 0);
+        origin = look_from;
+
+        w = unit_vector(look_from - look_at);
+        u = unit_vector(cross(vup, w));
+        v = cross(w, u);
+
+        lower_left_corner = origin - (half_width * focal_distance * u) - (half_height * focal_distance * v) - (focal_distance * w);
+        horizontal = 2.0 * half_width * focal_distance * u;
+        vertical = 2.0 * half_height * focal_distance * v;
     }
 
-    Ray get_ray(const float u, const float v) {
-        return Ray(origin, lower_left_corner + (u * horizontal) + (v * vertical) - origin);
+    Ray get_ray(const float s, const float t) {
+        vec3 disk = lens_radius * RandomInUnitDisk();
+        vec3 offset = (u * disk.x()) + (v * disk.y());
+        return Ray(origin + offset, lower_left_corner + (s * horizontal) + (t * vertical) - origin - offset);
     }
 
     vec3 origin;                ///< Origin
     vec3 lower_left_corner;     ///< Lower left corner of the scene
     vec3 horizontal;            ///< Horizontal axis
     vec3 vertical;              ///< Vertical axis
+    float lens_radius;          ///< Lens radius in pixels
+    vec3 u;                     ///< X axis vector
+    vec3 v;                     ///< Y axis vector
+    vec3 w;                     ///< Z axis vector
 };
 
 #endif//CAMERA_H

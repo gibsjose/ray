af30d1047a37f7c01de4b99261b5e3b2957228fd
Joe Gibson <gibsjose@mail.gvsu.edu>
2019-03-04 19:45:23 -0500

    Add method to generate a random scene and use new camera

--- src/main.cpp
+++ src/main.cpp
@@ -36,15 +36,24 @@
 ///////////////////////////////////////////////////////////////////////////////
 // METHODS
 ///////////////////////////////////////////////////////////////////////////////
+Hittable * RandomScene();
+
 int main() {
-    const uint32_t width = 1200;                        ///< Scene width
-    const uint32_t height = 600;                        ///< Scene height
-    const uint32_t num_samples = 80;                    ///< Number of samples over which to average edge colour
-    const float aspect_ratio = (float)width / height;   ///< Aspect ratio
-    const float vertical_fov = 90.0;                    ///< Vertical field of view in degrees
-    const float gamma = 2.0;                            ///< Gamma value
-    const float inv_gamma = 1.0 / gamma;                ///< Inverse gamma value
-    const uint8_t PNG_RGB_CHANNELS = 3;                 ///< Number of channels for PNG: 3 for RGB, 4 for RGBA
+    const uint32_t width = 1200;                ///< Scene width
+    const uint32_t height = 800;                ///< Scene height
+    const uint32_t num_samples = 80;            ///< Number of samples over which to average edge colour
+    const float gamma = 2.0;                    ///< Gamma value
+    const float inv_gamma = 1.0 / gamma;        ///< Inverse gamma value
+    const uint8_t PNG_RGB_CHANNELS = 3;         ///< Number of channels for PNG: 3 for RGB, 4 for RGBA
+
+    // Camera settings
+    const vec3 look_from(13, 2, 3);                                 ///< Look-from vector (origin)
+    const vec3 look_at(0, 0, 0);                                    ///< Look-at vector
+    const vec3 vup(0, 1, 0);                                        ///< View up
+    const float aspect_ratio = (float)width / height;               ///< Aspect ratio
+    const float vertical_fov = 20.0;                                ///< Vertical field of view in degrees (default = 90.0)
+    const float aperture = 0.1;                                     ///< Aperture
+    const float focal_distance = 10.0;                              ///< Focal distance
 
     uint8_t * image_data = NULL;
 
@@ -52,7 +61,7 @@ int main() {
     image_data = (uint8_t *) malloc(width * height * PNG_RGB_CHANNELS);
 
     // Camera object
-    Camera camera(vertical_fov, aspect_ratio);
+    Camera camera(look_from, look_at, vup, vertical_fov, aspect_ratio, aperture, focal_distance);
 
     // Materials
     Lambertian diffuse_1(vec3(0.1, 0.2, 0.5));
@@ -69,6 +78,8 @@ int main() {
     list[3] = new Sphere(vec3(-1, 0, -1), 0.5, &dielectric_1);
     list[4] = new Sphere(vec3(-1, 0, -1), -0.45, &dielectric_1);
     Hittable * world = new HittableList(list, 5);
+
+    world = RandomScene();
 
     // Draw screen starting in the lower left corner
     for (int32_t j = height - 1; j >= 0; --j) {
@@ -109,4 +120,40 @@ int main() {
 
     // Free the image data
     free(image_data);
+}
+
+Hittable * RandomScene() {
+    int32_t n = 500;
+    Hittable ** list = new Hittable * [n + 1];
+    list[0] =  new Sphere(vec3(0,-1000,0), 1000, new Lambertian(vec3(0.5, 0.5, 0.5)));
+    int32_t i = 1;
+    for (int32_t a = -11; a < 11; a++) {
+        for (int32_t b = -11; b < 11; b++) {
+            float material = drand48();
+            vec3 centre(a + 0.9 * drand48(), 0.2, b + 0.9 * drand48()); 
+            if ((centre - vec3(4, 0.2, 0)).length() > 0.9) {
+                // Diffuse 
+                if (material < 0.8) {
+                    list[i++] = new Sphere(centre, 0.2, new Lambertian(vec3(drand48() * drand48(), drand48() * drand48(), drand48() * drand48())));
+                }
+
+                // Metal
+                else if (material < 0.95) {
+                    list[i++] = new Sphere(centre, 0.2,
+                            new Metal(vec3(0.5 * (1 + drand48()), 0.5 * (1 + drand48()), 0.5 * (1 + drand48())),  0.5 * drand48()));
+                }
+                
+                // Glass
+                else {
+                    list[i++] = new Sphere(centre, 0.2, new Dielectric(1.5));
+                }
+            }
+        }
+    }
+
+    list[i++] = new Sphere(vec3(0, 1, 0), 1.0, new Dielectric(1.5));
+    list[i++] = new Sphere(vec3(-4, 1, 0), 1.0, new Lambertian(vec3(0.4, 0.2, 0.1)));
+    list[i++] = new Sphere(vec3(4, 1, 0), 1.0, new Metal(vec3(0.7, 0.6, 0.5), 0.0));
+
+    return new HittableList(list, i);
 }
